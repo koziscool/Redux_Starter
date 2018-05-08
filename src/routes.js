93277f359d99ec55c90c921801a46c9af1ebad9d
John Kosmicke <kozjob@yahoo.com>
2018-05-08 15:06:42 -0700

    empty courses page in

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-// import CoursesPage from './components/course/CoursesPage';
+import CoursesPage from './components/course/CoursesPage';
 // import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 import AboutPage from './components/about/AboutPage';
 
@@ -10,5 +10,6 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="about" component={AboutPage}/>
+    <Route path="courses" component={CoursesPage}/>
   </Route>
 );
