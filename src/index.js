ca614351d54c75a66d360a49ce21551b16f440f6
John Kosmicke <kozjob@yahoo.com>
2018-05-07 22:22:43 -0700

    finished config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 
-console.log('tetris');
+// console.log('tetris');
+
---
+++ tools/startMessage.js
@@ -0,0 +1,9 @@
+
+
+import colors from 'colors';
+
+/*  eslint-disable no-console */
+
+console.log( 'Starting app in dev mode'.yellow);
+
+
