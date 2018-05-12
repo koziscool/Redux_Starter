abe6801bdde3680e4ef2de2be7605eab14b593d7
John Kosmicke <kozjob@yahoo.com>
2018-05-12 06:02:32 -0700

    first cut manage course form, scaffoling for components, no funcationality yet

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,7 +3,6 @@ import * as types from '../actions/actionTypes';
 
 
 export default function courseReducer(state = [], action) {
-  console.log( action );
   switch(action.type){
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
