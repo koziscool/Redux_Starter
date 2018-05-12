15ba635f49710de4297bba4fabe23dab73465f6f
John Kosmicke <kozjob@yahoo.com>
2018-05-12 06:19:27 -0700

    next cut manage course form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,28 +2,40 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
-// import CourseForm from './CourseForm';
+import CourseForm from './CourseForm';
 // import {authorsFormattedForDropdown} from '../../selectors/selectors';
 // import toastr from 'toastr';
 
 export class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign( {}, this.props.course ),
+      errors: {}
+    };
+
   }
 
   render() {
     return (
-      <div>manage course</div>
+      <CourseForm
+        allAuthors= {[]}
+        course={this.state.course} 
+        errors={this.state.errors}         
+       />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+  let course = { id: 'tetris', watchHref: '', title: '', authorId: '', length: '', category: '' };
   return {
-    state: state
+    course: course
   };
 }
 
