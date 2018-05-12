a68deba5b8dd82de22a8f0a8416970562650f118
John Kosmicke <kozjob@yahoo.com>
2018-05-12 15:49:54 -0700

    first cut preloader component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,6 +2,7 @@
 
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
+import LoadingDots from './LoadingDots';
 
 const Header = ({loading}) => {
   return (
@@ -11,6 +12,7 @@ const Header = ({loading}) => {
       <Link to="/courses" activeClassName="active">Courses</Link>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
+      <LoadingDots interval={100} dots={20} />
     </nav>
   );
 };
