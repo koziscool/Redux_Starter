5a3914664fd059c3692bd0455ac65e849c08ebfb
John Kosmicke <kozjob@yahoo.com>
2018-05-12 19:58:53 -0700

    first cut application testing

---
+++ src/components/course/CourseForm.test.js
@@ -0,0 +1,28 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+function setup(){
+
+  let props = {
+    course: {}, saving: false, errors: {}, allAuthors: [],
+    onSave: () => {},
+    onChange: () => {}
+  };
+
+  let renderer = TestUtils.createRenderer();
+  renderer.render(<CourseForm {...props}/>);
+  let output = renderer.getRenderOutput();
+
+  return { props, output, renderer };
+}
+
+
+describe('CourseForm via react test utils', () => {
+  it( 'renders form and h1', ()=>{
+    const {output} = setup();
+
+    expect( output.type ).toBe('form');
+  });
+});
